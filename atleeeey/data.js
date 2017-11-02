0a9bf99962524c06e5d0d3cd972b1210a69419b5
AltSciFi <altscifi2@mail.com>
2017-11-01 23:10:37 -0400

    Added atleeeey.

---
+++ atleeeey/data.js
@@ -0,0 +1,81 @@
+<!--
+
+   username = "atleeeey";
+   user_full_name = "ash♡";
+
+   user_short_description = "MUA ‣ LOS ANGELES";
+   user_long_description = 'Merch available at <a href="http://www.atleeeey.co">www.atleeeey.co</>';
+
+   user_avatar_image = 'hb2QpyiJ_400x400.jpg';
+
+   user_gram_service = 'Instagram';
+   user_gram_page_url = 'https://www.instagram.com/atleeeey/';
+
+   user_tube_service = 'Youtube';
+   user_tube_page_url = 'https://www.youtube.com/user/atleeeey/videos?disable_polymer=1';
+
+   user_twitter_page_url = 'https://twitter.com/atleeeey';
+   user_snapchat_page_url = 'https://www.snapchat.com/add/atleeeey';
+   user_periscope_page_url = '#';
+
+   timeline_array = [
+		     {item_type:'gram', item_data:{filename:'23161640_1506193179428104_6080028207560523776_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/Ba9V7XQAuo5/?taken-by=atleeeey',}},
+		     {item_type:'gram', item_data:{filename:'23161511_162307474358963_356167271191674880_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/Ba6yx9ng3tx/?taken-by=atleeeey',}},
+		     {item_type:'gram', item_data:{filename:'22860947_342355509560874_6126589058958229504_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BazZrJoAcTG/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'CREEPY LIL MONSTER : ASHTOBERFEST | atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=5uSjaNckDTo',}},
+		     {item_type:'gram', item_data:{filename:'22802533_182528598976875_1107003352115314688_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaxNUr7AesX/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'JEWELED SKULL : ASHTOBERFEST | atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=jIUHsUSxJvs',}},
+		     {item_type:'tube', item_data:{title:'a little update',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=wvM0clXB7Do',}},
+		     {item_type:'tube', item_data:{title:'HAUNTED PUMPKIN HALLOWEEN MAKEUP : ASHTOBERFEST DAY 21/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=KGn3O4lB12Q',}},
+		     {item_type:'tube', item_data:{title:'GLAM POISON IVY HALLOWEEN MAKEUP : ASHTOBERFEST DAY 20/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=nN83lynaC4Y',}},
+		     {item_type:'gram', item_data:{filename:'22636881_532984460382154_6541548148894990336_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BafQsyDgX48/?taken-by=atleeeey',}},
+		     {item_type:'gram', item_data:{filename:'22638673_291131488040474_5960554075790508032_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaewpW7AFtT/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'UV SKELETON HALLOWEEN MAKEUP : ASHTOBERFEST DAY 19/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=drH43icp7lQ',}},
+		     {item_type:'gram', item_data:{filename:'22639372_147195069354362_5025994442293641216_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/Bac4Xb_gCG-/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'SNOTGIRL MAKEUP TUTORIAL : ASHTOBERFEST DAY 18/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=s6D_VWG-XkA',}},
+		     {item_type:'gram', item_data:{filename:'22582652_2012433315705452_1833636746865147904_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaaBZH9AiYP/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'POST MALONE MAKEUP TUTORIAL : ASHTOBERFEST DAY 17/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=J_awHx-43q0',}},
+		     {item_type:'gram', item_data:{filename:'22580172_531816110495562_6827118222303559680_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaXIUrEgleT/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'SCENE KING & QUEEN GRWM : ASHTOBERFEST DAY 16/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=MYcq2UuMxbk',}},
+		     {item_type:'gram', item_data:{filename:'22580844_1493890724053816_2085340084021755904_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaU4LX7glzx/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'WICKED WITCH HALLOWEEN MAKEUP : ASHTOBERFEST DAY 15/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=5tD9Jmkzhv4',}},
+		     {item_type:'gram', item_data:{filename:'22581992_685270954998819_5591513365978546176_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaSP5q0gL2C/?taken-by=atleeeey',}},
+		     {item_type:'tube', item_data:{title:'RAVEN & STARFIRE HALLOWEEN GRWM & @sparksss : ASHTOBERFEST DAY 14/31| atleeeey',
+						   service:'youtube',
+						   tube_url:'https://www.youtube.com/watch?v=YuM0Bt9oy9w',}},
+		     {item_type:'gram', item_data:{filename:'22500310_1692213547518338_4446348164286382080_n.jpg',
+						   instagram_url:'https://www.instagram.com/p/BaQLvXlA0rc/?taken-by=atleeeey',}},
+		     ]
+
+//-->
